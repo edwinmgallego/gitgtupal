686eed00ad5555140238e0ddea91182115fb6182
FellDev0303 <josejose2868@hotmail.com>
2025-10-06 10:36:51 -0500

    Este es el primer commit

---
+++ Documents/NetBeansProjects/GitGrupal/src/main/java/com/uao/gitgrupal/GitGrupal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.gitgrupal;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class GitGrupal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
