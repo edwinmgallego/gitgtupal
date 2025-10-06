9b7113b7aad27f0ac805dbf7596dc4126f40252b
juanka0763 <juanka0763@gmail.com>
2025-10-06 16:00:26 -0500

    Mi primera clase y paquete en git

---
+++ src/main/java/Juan_Herrera/Juan_Herrera.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Juan_Herrera;
+
+/**
+ *
+ * @author juank
+ */
+public class Juan_Herrera {
+    
+}
