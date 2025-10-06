79d06741c15a8aac567a657985d7ddb1908ee78a
emgallego <emgallego@D122539.uao.edu.co>
2025-10-06 10:36:50 -0500

    este es mi primer commit

---
+++ src/main/java/com/mycompany/gitgrupal/GitGrupal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitgrupal;
+
+/**
+ *
+ * @author emgallego
+ */
+public class GitGrupal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
