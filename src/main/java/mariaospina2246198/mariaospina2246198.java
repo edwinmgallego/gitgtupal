cadffb0b3817c33acb28de2f9752a1896545cbc8
mxom671 <maria_val.ospina@uao.edu.co>
2025-10-10 14:40:45 -0500

    hice un paquete con mi nombre y una clase con mi nombre.

---
+++ src/main/java/mariaospina2246198/mariaospina2246198.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mariaospina2246198;
+
+/**
+ *
+ * @author luisb
+ */
+public class mariaospina2246198 {
+    
+}
